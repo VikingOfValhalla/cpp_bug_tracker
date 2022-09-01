986c215ed0d60e3a01da3a266ac82864ea3e093b
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2022-09-01 12:20:12 -0400

    Completed front end prototype.

--- cpp_bug_tracker/mainwindow.cpp
+++ cpp_bug_tracker/mainwindow.cpp
@@ -13,9 +13,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_pushButton_clicked()
 {
     ui->tableWidget->insertRow(0);
+}
+
+
+void MainWindow::on_delete_ticket_clicked()
+{
+    int row = ui->tableWidget->currentRow();
+    ui->tableWidget->removeRow(row);
 }
 
